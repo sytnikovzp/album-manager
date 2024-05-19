9b04f1d639e9876eae5d314a5344cfa290dc1c69
alex <sytnikov.zp@gmail.com>
2024-05-19 10:12:06 +0300

    Add all reducers

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,13 @@ import { logger } from 'redux-logger';
 
 import albumsReducer from './slices/albumSlice';
 import photosReducer from './slices/photosSlice';
+import usersReducer from './slices/usersSlice';
 
 export default configureStore({
-    reducer: {
-        albumsList: albumsReducer,
-        photosList: photosReducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
-})
+  reducer: {
+    albumsList: albumsReducer,
+    photosList: photosReducer,
+    usersList: usersReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
