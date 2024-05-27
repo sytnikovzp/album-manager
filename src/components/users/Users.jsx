b837b91eda123e4bc40bc4122bfb30c13ade757a
alex <sytnikov.zp@gmail.com>
2024-05-27 15:34:34 +0300

    Remove unnecessary line 'import AlbumPhotos'

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,31 +1,23 @@
-import {
-	Routes,
-	Route,
-	NavLink,
-	Navigate,
-} from 'react-router-dom';
+import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 import UserForm from './UserForm';
-import AlbumPhotos from '../albums/AlbumPhotos';
 import UserAlbums from './UserAlbums';
 import UsersList from './UsersList';
 
 function Users() {
-
-	return (
-		<>
-			<nav>
-				<NavLink to='add'>Add</NavLink>
-			</nav>
-			<hr />
-			<Routes>
-				<Route path='add/:id' element={<UserForm />} />
-				<Route path='add' element={<Navigate to=':id'/>} />
-				<Route path='/albums/:id' element={<AlbumPhotos />}/>
-				<Route path=':id' element={<UserAlbums />}/>
-				<Route path='/' element={<UsersList />}/>
-			</Routes>
-		</>
-	);
+  return (
+    <>
+      <nav>
+        <NavLink to='add'>Add</NavLink>
+      </nav>
+      <hr />
+      <Routes>
+        <Route path='add/:id' element={<UserForm />} />
+        <Route path='add' element={<Navigate to=':id' />} />
+        <Route path=':id' element={<UserAlbums />} />
+        <Route path='/' element={<UsersList />} />
+      </Routes>
+    </>
+  );
 }
 
 export default Users;
