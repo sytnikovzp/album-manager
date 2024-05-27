b379d89cbd45204a5d0cbb41e3db1e82636316d3
alex <sytnikov.zp@gmail.com>
2024-05-27 03:26:26 +0300

    Add Formik & Yup to project

--- src/components/users/UserForm.jsx
+++ src/components/users/UserForm.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useNavigate } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import { emptyUser } from '../../constants';
 import { createUser, updateUser } from '../../store/slices/usersSlice';
 import './UserForm.css';
@@ -16,111 +17,108 @@ function UserForm() {
 
   const currentUser = users.find((user) => user.id === Number(id));
 
-  const [editUser, setEditUser] = useState(
-    currentUser ? currentUser : emptyUser
-  );
-
-  const onInputChange = (event) => {
-    setEditUser({
-      ...editUser,
-      [event.target.name]: event.target.value,
-    });
-  };
-
   const goHome = () => {
     navigate('/');
   };
 
-  const onReset = (event) => {
-    event.preventDefault();
-    setEditUser(emptyUser);
+  const schema = Yup.object().shape({
+    address: Yup.object().shape({
+      city: Yup.string().required('City is required field'),
+      street: Yup.string().required('Street is required field'),
+    }),
+    name: Yup.string()
+      .min(3, 'Too less symbols')
+      .max(20, 'Too many symbols')
+      .required('Name is required field'),
+    email: Yup.string().email().required('Email is required field'),
+  });
+
+  const onFormSubmit = (values) => {
+    !values.id ? dispatch(createUser(values)) : dispatch(updateUser(values));
   };
 
-  const onFormSubmit = (event) => {
-    event.preventDefault();
-    !editUser.id
-      ? dispatch(createUser(editUser))
-      : dispatch(updateUser(editUser));
+  const renderForm = ({ isValid }) => {
+    return (
+      <Form id='users-form' onSubmit={onFormSubmit}>
+        <div className='field-container'>
+          <label htmlFor='name'>Name</label>
+          <Field type='text' name='name' id='name' />
+        </div>
+        <ErrorMessage name='name'>
+          {(msg) => <div className='error'>{msg}</div>}
+        </ErrorMessage>
+        <fieldset id='contact' form='users-form' className='group-container'>
+          <legend>Contact</legend>
+          <div className='field-container'>
+            <label htmlFor='email'>E-mail</label>
+            <Field type='email' name='email' id='email' placeholder='email' />
+          </div>
+          <ErrorMessage name='email'>
+            {(msg) => <div className='error'>{msg}</div>}
+          </ErrorMessage>
+          <div className='field-container'>
+            <label htmlFor='phone'>Phone</label>
+            <Field type='text' name='phone' id='phone' placeholder='phone' />
+          </div>
+        </fieldset>
+
+        <fieldset id='contact' form='users-form' className='group-container'>
+          <legend>Address</legend>
+          <div className='field-container'>
+            <label htmlFor='city'>City</label>
+            <Field
+              type='text'
+              name='address.city'
+              id='city'
+              placeholder='city'
+            />
+          </div>
+          <ErrorMessage name='address.city'>
+            {(msg) => <div className='error'>{msg}</div>}
+          </ErrorMessage>
+          <div className='field-container'>
+            <label htmlFor='street'>Street</label>
+            <Field
+              type='text'
+              name='address.street'
+              id='street'
+              placeholder='street'
+            />
+          </div>
+          <ErrorMessage name='address.street'>
+            {(msg) => <div className='error'>{msg}</div>}
+          </ErrorMessage>
+          <div className='field-container'>
+            <label htmlFor='zipcode'>Zipcode</label>
+            <Field
+              type='text'
+              name='address.zipcode'
+              id='zipcode'
+              placeholder='zipcode'
+            />
+          </div>
+        </fieldset>
+        <div className='btn-group'>
+          <button type='submit' disabled={!isValid}>
+            Save
+          </button>
+          <button type='reset'>Reset</button>
+          <button type='button' onClick={goHome}>
+            Return
+          </button>
+        </div>
+      </Form>
+    );
   };
 
   return (
-    <form id='users-form' onSubmit={onFormSubmit}>
-      <div className='field-container'>
-        <label>Name</label>
-        <input
-          type='text'
-          name='name'
-          value={editUser.name}
-          onChange={onInputChange}
-        />
-      </div>
-      <fieldset id='contact' form='users-form' className='group-container'>
-        <legend>Contact</legend>
-        <div className='field-container'>
-          <label>E-mail</label>
-          <input
-            type='email'
-            name='email'
-            value={editUser.email}
-            onChange={onInputChange}
-            placeholder='email'
-          />
-        </div>
-        <div className='field-container'>
-          <label>Phone</label>
-          <input
-            type='text'
-            name='phone'
-            value={editUser.phone}
-            onChange={onInputChange}
-            placeholder='phone'
-          />
-        </div>
-      </fieldset>
-
-      <fieldset id='contact' form='users-form' className='group-container'>
-        <legend>Address</legend>
-        <div className='field-container'>
-          <label>City</label>
-          <input
-            type='text'
-            name='city'
-            value={editUser.address.city}
-            onChange={onInputChange}
-            placeholder='city'
-          />
-        </div>
-        <div className='field-container'>
-          <label>Street</label>
-          <input
-            type='text'
-            name='street'
-            value={editUser.address.street}
-            onChange={onInputChange}
-            placeholder='street'
-          />
-        </div>
-        <div className='field-container'>
-          <label>Zipcode</label>
-          <input
-            type='text'
-            name='zipcode'
-            value={editUser.address.zipcode}
-            onChange={onInputChange}
-            placeholder='zip code'
-          />
-        </div>
-      </fieldset>
-      <div className='btn-group'>
-        <button type='submit'>Save</button>
-        <button type='button' onClick={onReset}>
-          Reset
-        </button>
-        <button type='button' onClick={goHome}>
-          Return
-        </button>
-      </div>
-    </form>
+    <Formik
+      initialValues={currentUser ? currentUser : emptyUser}
+      onSubmit={onFormSubmit}
+      validationSchema={schema}
+    >
+      {renderForm}
+    </Formik>
   );
 }
 
