619e817f210d288e75d484ef377407622b080ef7
alex <sytnikov.zp@gmail.com>
2024-05-29 01:54:33 +0300

    Add AppBar & add custom theme

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,15 @@ import {
   Route,
   Navigate,
 } from 'react-router-dom';
+// ==========================
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+// ==========================
 import './App.css';
 import Albums from './components/albums/Albums';
 import Users from './components/users/Users';
@@ -13,9 +22,38 @@ import { setActive } from './services/styleService';
 import './App.css';
 
 function App() {
-
   return (
     <Router>
+      <Stack
+        sx={{
+          flexGrow: 1,
+          width: '900px',
+        }}
+      >
+        <AppBar position='static'>
+          <Toolbar>
+            <IconButton
+              size='large'
+              edge='start'
+              color='inherit'
+              aria-label='open drawer'
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant='h6'
+              noWrap
+              component='div'
+              fontSize={20}
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
+              Album manager
+            </Typography>
+            <Button color='inherit'>Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Stack>
       <div className='header'>
         <ul>
           <li>
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material';
 import store from './store';
 import App from './App.jsx';
 import './index.css';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#DBD9DB',
+      light: '#D6C8E1',
+      dark: '#82A285',
+    },
+  },
+  typography: {
+    fontSize: 15,
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
